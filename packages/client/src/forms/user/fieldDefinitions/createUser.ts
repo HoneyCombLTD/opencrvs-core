5702122437cf083bb0c2b4c4f05f18cef4a30c36
anyric <anyamaronyango@gmail.com>
2024-03-27 15:13:53 +0300

    update signature expression

--- packages/client/src/forms/user/fieldDefinitions/createUser.ts
+++ packages/client/src/forms/user/fieldDefinitions/createUser.ts
@@ -224,8 +224,7 @@ export function userSectionFormType(): ISerializedFormSection {
         conditionals: [
           {
             action: 'hide',
-            expression:
-              '!import.meta.env.VITE_SIGNATURE_REQUIRED_FOR_ROLES.includes(values.systemRole)'
+            expression: import.meta.env.VITE_SIGNATURE_REQUIRED_FOR_ROLES.includes('values.systemRole')
           }
         ],
         fields: [
